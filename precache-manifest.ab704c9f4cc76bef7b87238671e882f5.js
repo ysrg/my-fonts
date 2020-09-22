0d49103db0daa8e7dc9adbe078a44b5bcd9373dd
sergeym-mac <c_smoraru@zipwhip.com>
2020-09-22 20:56:17 +0300

    Updates

---
+++ precache-manifest.ab704c9f4cc76bef7b87238671e882f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c225b73a68a4293edb240eef5010e315",
+    "url": "/my-fonts/index.html"
+  },
+  {
+    "revision": "8787d5f08f1b1bad581d",
+    "url": "/my-fonts/static/js/2.d166e647.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-fonts/static/js/2.d166e647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b877e063b7a1e835999b",
+    "url": "/my-fonts/static/js/main.000a32be.chunk.js"
+  },
+  {
+    "revision": "01b89e54113d5aa922ea",
+    "url": "/my-fonts/static/js/runtime-main.1c670eb6.js"
+  }
+]);
